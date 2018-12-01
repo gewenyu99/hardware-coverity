c2bad15f17c3e30dd6c8216a553e24b2ff7033b4
Wen Yu Ge <gewenyu99@gmail.com>
2018-11-30 21:55:40 -0500

    made some hilarious fake data :')

--- hardware-coverity-go/main.go
+++ hardware-coverity-go/main.go
@@ -1,5 +1,13 @@
 package main
 
+import "github.com/olivere/elastic"
+import "github.com/gewenyu99/hardware-coverity/hardware-coverity-go/sample-data"
+
 func main() {
-	
+	esClient, err := elastic.NewClient(elastic.SetURL("http://192.168.2.10:9201"))
+	if err != nil {
+		// Handle error
+	}
+	sample_data.LoadSample(esClient)
+
 }
---
+++ hardware-coverity-go/sample-data/loader.go
@@ -0,0 +1,6 @@
+package sample_data
+
+import "github.com/olivere/elastic"
+
+func LoadSample(esClient *elastic.Client){
+}
